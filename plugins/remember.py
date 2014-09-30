c724b0a1a1e5944489475cd4005b92495e8205a0
Kevin Chung <kchung@nyu.edu>
2014-09-30 12:35:53 -0400

    Making remember command stack data

--- plugins/remember.py
+++ plugins/remember.py
@@ -4,21 +4,19 @@ remember.py: written by Scaevolus 2010
 
 import string
 import re
-
+import random
 from util import hook
 
 
 def db_init(db):
-    db.execute("create table if not exists memory(chan, word, data, nick,"
-               " primary key(chan, word))")
+    db.execute("create table if not exists memory(chan, word, data, nick)")
     db.commit()
 
 
 def get_memory(db, chan, word):
-    row = db.execute("select data from memory where chan=? and word=lower(?)",
-                     (chan, word)).fetchone()
-    if row:
-        return row[0]
+    row = db.execute("select data from memory where chan=? and word=lower(?)", (chan, word)).fetchall()
+    if row > 0:
+        return random.choice(row)[0]
     else:
         return None
 
@@ -38,50 +36,10 @@ def remember(inp, nick='', chan='', db=None):
     except ValueError:
         return remember.__doc__
 
-    data = get_memory(db, chan, head)
-    if data is not None:
-        _head, _tail = data.split(None, 1)
-    else:
-        _head, _tail = head, ''
-
-    if tail[0] == '+':
-        append = True
-        # ignore + symbol
-        new = tail[1:]
-        # data is stored with the input so ignore it when re-adding it
-        if len(tail) > 1 and tail[1] in (string.punctuation + ' '):
-            tail = _tail + new
-        else:
-            tail = _tail + ' ' + new
-
-    if len(tail) > 2 and tail[0] == 's' and tail[1] in string.punctuation:
-        args = tail.split(tail[1])
-        if len(args) == 4 and args[3] == '':
-            args = args[:-1]
-        if len(args) == 3:
-            replacement = True
-            _, src, dst = args
-            new_data = _tail.replace(src, dst, 1)
-            if new_data == _tail:
-                return 'replacement left data unchanged'
-            tail = new_data
-        else:
-            return 'invalid replacement syntax -- try s$foo$bar instead?'
-
-    db.execute("replace into memory(chan, word, data, nick) values"
-               " (?,lower(?),?,?)", (chan, head, tail, nick))
+    db.execute("insert into memory(chan, word, data, nick) values (?,lower(?),?,?)", (chan, head, tail, nick))
     db.commit()
 
-    if data:
-        if append:
-            return "appending %s to %s" % (new, data.replace('"', "''"))
-        elif replacement:
-            return "replacing '%s' with '%s' in %s" % (src, dst, _tail)
-        else:
-            return 'forgetting "%s", remembering this instead.' % \
-                data.replace('"', "''")
-    else:
-        return 'done.'
+    return 'done.'
 
 
 @hook.command
@@ -111,4 +69,5 @@ def question(inp, chan='', say=None, db=None):
 
     data = get_memory(db, chan, inp.group(1).strip())
     if data:
+        print data
         say(data)
