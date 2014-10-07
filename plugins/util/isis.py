471de971468e96b44a59c8a249de13abadfb2eff
Kevin Chung <kchung@nyu.edu>
2014-10-06 22:02:29 -0400

    Case insensitive select query

--- plugins/util/isis.py
+++ plugins/util/isis.py
@@ -1,6 +1,6 @@
 def handle_to_email(conn, handle):
     db = conn.cursor()
-    row = db.execute("SELECT email FROM user WHERE handle == ?", (handle,)).fetchone()
+    row = db.execute("SELECT email FROM user WHERE handle == ? COLLATE NOCASE", (handle,)).fetchone()
     if row:
         return row[0]
     else:
