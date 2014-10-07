9748f79b1f5a88ba383526f8c19453fc83ba6213
Kevin Chung <kchung@nyu.edu>
2014-10-06 20:44:30 -0400

    Finishing up the who command

--- plugins/who.py
+++ plugins/who.py
@@ -2,7 +2,13 @@ from util import hook
 from util import isis as irc
 
 @hook.command
-def who(inp, nick="", chan="", isis=None):
-    row = irc.handle_to_email(isis, nick)
-    if row:
-        return row
+def who(inp, nick="", msg="", chan="", isis=None):
+    try:
+        handle = msg.split(" ")[1]
+        row = irc.handle_to_email(isis, handle)
+        if row:
+            return row
+        else:
+            return "idk that person"
+    except:
+        return "idk that person"
